938b6f9f1ce0bb0b2ce207a1ab2c8985398a2e99
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-05-22 11:19:32 -0300

    Create style.js

---
+++ src/pages/Proprietario/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        maxHeight: '800px',
+        overflow: 'auto'
+    },
+    text: {
+        marginTop: 25,
+        marginBottom: 15,
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    input: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5,
+        wid
+    }
+})
