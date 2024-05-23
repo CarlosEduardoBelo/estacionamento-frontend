39ddf43a3159c889d29057ac1ead5a2ca28d4774
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-05-23 13:55:01 -0300

    Alinhamento home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,7 +31,9 @@ const Home = ({ navigation }) => {
 };
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        marginTop: 100
+        //justifyContent: "center"
     },
     imageContainer: {
         flex: 0.4,
